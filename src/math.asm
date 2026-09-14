b758506c89e8ac8970ed84cf37f8cd96ba3cdf59
Daniel Weiner <danielbweiner@gmail.com>
2026-09-14 14:37:52 -0500

    Optimize division by 100

--- src/math.asm
+++ src/math.asm
@@ -15,29 +15,27 @@ DivHLBy10:      ; Divides HL in place by 10, remainder in A, destroys BC
     ; A contains three leading bits. When shifting another bit in, 10 can be subtracted from A at most 1 time
     LD   B, 13 ; 16 iterations minus the first three which are already done
     LD   C, 10 ; Divisor for the division by 10
-__DivHLBy10Loop:
+__LongDivisionLoop:
     ADD  HL, HL ; shift another 0 into the quotient portion of HL
     RLA         ; shift a bit into remainder and check if 10 can be subtracted
+__LongDivisionCheck:
     CP   C      ; if 10 can be subtracted from remainder, subtract it and set the quotient bit to 1
     JR   C, $+4 ; otherwise, skip both the subtraction and the increment of the quotient bit
     SUB  C      ; subtract 10 from remainder
     INC  L      ; quotient bit is now 1
-    DJNZ __DivHLBy10Loop
+    DJNZ __LongDivisionLoop
     RET
 
-DivHLBy100:
-    XOR A     ; clear out carry flags for the RRA that follows
-              ; similar to division by 10, shift 7 bits of HL into A (first 7 bits are always 0 when dividing by 100)
-    LD A, H   ; start by moving HL 8 bits left into remainder
-    LD H, L
-    LD L, 0
-              ; Then rewind the shift right 1 bit, resulting in a shift of 7
-    RRA       ; shift remainder 1 bit right
-    RR H      ; propagate the carry from remainder into H
-    RR L      ; and again into L
-    LD B, 9   ; initialize loop counter and divisor for division by 100
-    LD C, 100
-    JR __DivHLBy10Loop ; Reuse the same loop for division by 100
+DivHLBy100: ; Divides HL in place by 100, remainder in A, destroys BC
+    ; Similar to division by 10, we can shift HL into the remainder 7 times before subtraction
+    ; By shifting HL 8 times into the remainder, the subtraction check needs to be done once, followed by 8 more iterations
+    ; This allows us to jump into the main division loop at the comparison check after a simple shift of 8 bits
+    LD   A, H   ; start by moving HL 8 bits left into remainder
+    LD   H, L
+    LD   L, 0
+    LD   B, 9   ; Loop counter decrements after the initial check, and then runs 8 additional times
+    LD   C, 100
+    JR   __LongDivisionCheck ; Reuse the same loop for division by 100
 .endmodule ; Math
 
 #endif ; MATH_ASM
